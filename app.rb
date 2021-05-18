82f29f59da5546ea354f675cbae00b6cb0505dc7
yatsun <yatsunlam25@gmail.com>
2021-05-17 21:04:52 -0400

    commit tree 6cd77d78b105194cc6746269b5bb5fb74310022b

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/name' do
+        "My name is ['name']"
+    end
+
+    get '/hometown' do
+        "My hometown is ['hometown']"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is ['favorite-song']"
+    end
+    
 end
